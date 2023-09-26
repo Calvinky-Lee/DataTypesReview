79854b7443caae10e49a238b6df1a10605fdc28a
Calvin Lee <63270424+Calvinky-Lee@users.noreply.github.com>
2023-09-26 09:15:16 -0400

    Add files via upload

---
+++ Exersize 1.py
@@ -0,0 +1,9 @@
+width = 17
+height = 12.0
+delimiter = '.'
+
+print(width/2) #8.5 float
+print(width/2.0) #8.5 float
+print(height/3) #4.0 float
+print(1+2*5) #11 int
+print(delimiter * 5) # ..... string
